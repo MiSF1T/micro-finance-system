f0cb8d04945447e44bf762427d08ab6a1f4e5539
Raunak Bag <raunakbagh@gmail.com>
2020-07-30 02:47:41 +0530

    Beta 2.6
    FIXED/UPDATED:
    - Fixed loan page display alignment
    - Centered date properly
    - Updated overlay styling
    
    KNOWN ISSUES:
    - Potential loan insertion problem
    
    PENDING:
    - Logo
    - About Us

--- loan/scripts/main.js
+++ loan/scripts/main.js
@@ -7,11 +7,6 @@ var btn = document.getElementById("info");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-function update()
-{
-  principal = document.getElementById("amount").value;
-
-}
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -29,6 +24,23 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+//Define EMI calcuation function
+function emi()
+	{
+		if(document.getElementById('amount2').value==null || document.getElementById('amount2').value.length==0 || document.getElementById('installment2').value==null) 
+			{document.getElementById('emi1').value="Data Required.";}
+		else 
+			{
+				var emi='';
+				var princ1= document.getElementById('amount2').value;
+				var term1= document.getElementById('installment2').value;
+				var intr1=document.getElementById('rate').value / 1200;
+				document.getElementById('emi1').value =Math.round(princ1 * intr1 / (1-(Math.pow(1/(1 + intr1), term1)))*100)/100; 
+				document.getElementById('total').value= Math.round((document.getElementById('emi1').value * document.getElementById('installment2').value)*100)/100;
+				document.getElementById('total_inte').value=Math.round((document.getElementById('total').value*1 - document.getElementById('amount2').value*1)*100)/100;
+			}
+	}
 
 //Reset field values of overlay
 var res = document.getElementById("reset");
