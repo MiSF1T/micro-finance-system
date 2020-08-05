724b0f9d66593bf0c6ad9f38131ef59620bc16c7
Raunak Bag <raunakbagh@gmail.com>
2020-08-05 21:56:34 +0530

    Update connection.php
    Connection details for byethost31

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-    $db = new PDO("mysql:host=localhost;dbname=mfs","root","");
+    $db = new PDO("mysql:host=sql205.byethost.com;dbname=b31_26436622_mfs","b31_26436622","xxxunknown1");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
