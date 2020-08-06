5ef752f63feba8fc353a689d7ef0b7b2222592a3
Raunak Bag <raunakbagh@gmail.com>
2020-08-06 10:48:33 +0530

    release v1.4
    - Loan and TD lower than permissible amount is no longer created.
    - Updated common vars

--- common variables/common_var.php
+++ common variables/common_var.php
@@ -1,6 +1,9 @@
 <?php 
     $min_balance = 2000;                //Define minimum balance to be maintained for an account
+    $min_td_amount = 5000;              //Define minimum TD amount
+    $min_loan_amount = 10000;           //Define minimum loan amount
     $approval_percentage = 0.25;        //Define loan approval percentage
     $savingsinterest_date = 27;         //Define savings interest date
     $loanrepay_date = 5;                //Define auto loan repay date
+
 ?>
